9e3575791c363a374812f0000e0991d3f9133d9a
Maksim <mgrib03@yandex.ru>
2023-01-26 17:31:11 +0300

    Finished Ex4

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[4] { 6542, 897, 54, 3452 };
+int[] finalArray = new int[2];
+
+for (int i = 0; i < finalArray.Length; i++)
+ {
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j]<1000)
+        {
+             finalArray[i] = array[j];
+             int index = Array.IndexOf(array, array[j]);
+        array = array.Where((e, i) => i != index).ToArray();
+            break;
+        }
+    }
+ }
+var str = string.Join(" ", finalArray);
+Console.WriteLine(str);
